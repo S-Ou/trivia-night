cb216cbc79d2255387f4c781f50a1329a7c0dcb3
Samuel Ou <samuel.ou03@gmail.com>
2025-07-13 19:29:12 +1200

    feat: prisma database

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function seedConfig() {
+  const defaults = [{ name: "title", value: "Trivia Night" }];
+
+  for (const config of defaults) {
+    await prisma.config.upsert({
+      where: { name: config.name },
+      update: {},
+      create: config,
+    });
+  }
+}
+
+async function main() {
+  console.log("Seeding database...");
+
+  await seedConfig();
+
+  console.log("✅ Seeding complete");
+}
+
+main()
+  .catch((e) => {
+    console.error("❌ Seeding error:", e);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect());
