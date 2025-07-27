545a9908dd08e3b035afce33add173be9e3e2197
Samuel Ou <samuel.ou03@gmail.com>
2025-07-27 23:59:47 +1200

    feat: delete event in ui

--- src/app/[eventId]/config/page.tsx
+++ src/app/[eventId]/config/page.tsx
@@ -8,8 +8,13 @@ import {
 } from "@/components/ConfigForm";
 import { handleConfigUpdate } from "@/components/handleConfigUpdate";
 import { EventPageTemplate, Page } from "../pageTemplate";
+import { AlertDialog, Button, Flex, Separator } from "@radix-ui/themes";
+import { Skull, TriangleAlert } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export default function ConfigPage() {
+  const router = useRouter();
   const { event, isLoading, updateEvent } = useEventContext();
 
   const [title, setTitle] = useState("");
@@ -82,9 +87,61 @@ export default function ConfigPage() {
     },
   ];
 
+  function handleDeleteEvent() {
+    if (event.id) {
+      fetch(`/api/${event.id}/event`, {
+        method: "DELETE",
+      })
+        .then((response) => {
+          if (response.ok) {
+            toast.success("Event deleted successfully!");
+            router.push("/");
+          } else {
+            throw new Error("Failed to delete event");
+          }
+        })
+        .catch((error) => {
+          console.error("Error deleting event:", error);
+          toast.error("Failed to delete event.");
+        });
+    }
+  }
+
   return (
     <EventPageTemplate currentPage={Page.Config}>
       <ConfigForm fields={configFields} />
+
+      <Separator size="3" />
+
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">
+            <TriangleAlert size={18} />
+            Delete event
+            <Skull size={18} />
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Delete event</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure? This event will be permanently deleted and cannot be
+            recovered.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red" onClick={handleDeleteEvent}>
+                Delete event
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
     </EventPageTemplate>
   );
 }
