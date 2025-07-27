4f9e7a0127c68bd0cba4f7320b5426e0933a4b4c
Samuel Ou <samuel.ou03@gmail.com>
2025-07-27 17:02:07 +1200

    feat: save event ID

--- src/app/[eventId]/page.tsx
+++ src/app/[eventId]/page.tsx
@@ -1,16 +1,103 @@
 "use client";
 
-import { Button } from "@radix-ui/themes";
+import { Button, Separator, TextField } from "@radix-ui/themes";
 import { PageTemplate, Page } from "./pageTemplate";
-import { Presentation } from "lucide-react";
+import { Clipboard, Presentation } from "lucide-react";
 import { useEventId } from "@/contexts/EventIdContext";
+import { toast } from "sonner";
+import styled from "styled-components";
 
-export default function Home() {
+const CallToActionWrapper = styled.div`
+  max-width: 20rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const IdButton = styled.a`
+  color: #2563eb;
+  cursor: pointer;
+  font-weight: 800;
+  font-stretch: expanded;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const CallToActionText = styled.span`
+  text-align: center;
+  text-wrap: balance;
+`;
+
+const TextFieldRoot = styled(TextField.Root)`
+  background-image: none;
+  height: auto;
+  padding: 0.5rem;
+  width: 100%;
+`;
+
+const TextFieldSlot = styled(TextField.Slot)`
+  padding-right: 0;
+`;
+
+const StyledButton = styled(Button)`
+  padding-inline: 0.5rem;
+`;
+
+function CopyUrlButton() {
+  const url = window.location.href;
+  const handleCopy = () => {
+    navigator.clipboard.writeText(url).then(() => {
+      toast.success("URL copied to clipboard!");
+    });
+  };
+  return (
+    <TextFieldRoot disabled value={url} size="3" name="copy-url">
+      <TextFieldSlot side="right">
+        <StyledButton onClick={handleCopy}>
+          <Clipboard size={20} />
+        </StyledButton>
+      </TextFieldSlot>
+    </TextFieldRoot>
+  );
+}
+
+function CopyIdButton() {
   const { eventId } = useEventId();
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(eventId.toString()).then(() => {
+      toast.success("Event ID copied to clipboard!");
+    });
+  };
+
+  return (
+    <IdButton
+      onClick={(e) => {
+        e.preventDefault();
+        handleCopy();
+      }}
+    >
+      {eventId}
+    </IdButton>
+  );
+}
+
+export default function Home() {
   return (
     <PageTemplate currentPage={Page.Home}>
-      {eventId}
+      <CallToActionWrapper>
+        <CallToActionText>
+          Save this ID – <CopyIdButton /> – or the URL below to return to this
+          event later or share with others.
+        </CallToActionText>
+        <CopyUrlButton />
+      </CallToActionWrapper>
+
+      <Separator size="3" />
+
       <a href="./present" target="_blank" rel="noopener noreferrer">
         <Button variant="solid" size={"4"}>
           <Presentation />
