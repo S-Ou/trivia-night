720abd5d44919a5f7e29e16a7040d34c4acf80bc
Samuel Ou <samuel.ou03@gmail.com>
2025-08-01 18:29:07 +1200

    style: mobile presenting warning

--- src/app/[eventId]/page.tsx
+++ src/app/[eventId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Button, Separator } from "@radix-ui/themes";
-import { Presentation } from "lucide-react";
+import { Button, Callout, Separator } from "@radix-ui/themes";
+import { Info, Presentation } from "lucide-react";
 import { useEventId } from "@/contexts/EventIdContext";
 import { toast } from "sonner";
 import styled from "styled-components";
@@ -9,6 +9,14 @@ import { EventPageTemplate, Page } from "./pageTemplate";
 import Link from "next/link";
 import { useQuestionContext } from "@/contexts/QuestionContext";
 import { CallToActionCopyUrl } from "@/components/CopyUrlButton";
+import { useIsMobile } from "@/utils/isMobile";
+
+const PresentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+`;
 
 const IdButton = styled.a`
   color: var(--accent-11);
@@ -44,6 +52,7 @@ function CopyIdButton() {
 
 export default function Home() {
   const { questions } = useQuestionContext();
+  const isMobile = useIsMobile();
 
   return (
     <EventPageTemplate currentPage={Page.Home}>
@@ -51,19 +60,29 @@ export default function Home() {
         This is the event&apos;s unique ID: <CopyIdButton />. Save it somewhere
         safe to return to this event later or to share with others.
       </CallToActionCopyUrl>
-
       <Link href="/">
         <Button variant={"outline"}>Exit to homepage</Button>
       </Link>
-
       <Separator size="3" />
-
-      <a href="./present" target="_blank" rel="noopener noreferrer">
-        <Button variant="solid" size={"4"} disabled={questions.length === 0}>
-          <Presentation />
-          {questions.length > 0 ? "Present" : "Add questions to present"}
-        </Button>
-      </a>
+      <PresentWrapper>
+        <a href="./present" target="_blank" rel="noopener noreferrer">
+          <Button variant="solid" size={"4"} disabled={questions.length === 0}>
+            <Presentation />
+            {questions.length > 0 ? "Present" : "Add questions to present"}
+          </Button>
+        </a>
+        {isMobile && (
+          <Callout.Root size={"1"}>
+            <Callout.Icon>
+              <Info size={16} />
+            </Callout.Icon>
+            <Callout.Text>
+              Mobile not recommended for presenting, please use a desktop
+              device.
+            </Callout.Text>
+          </Callout.Root>
+        )}
+      </PresentWrapper>
     </EventPageTemplate>
   );
 }
