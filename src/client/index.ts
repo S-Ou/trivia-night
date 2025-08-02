2b7b7c04d7131b6d03d527df5c84bd9d0f596c18
Samuel Ou <samuel.ou03@gmail.com>
2025-08-02 18:24:22 +1200

    fix: some commented-out code fix attempt

--- src/client/index.ts
+++ src/client/index.ts
@@ -8,3 +8,21 @@ addEventUpdatedAtMiddleware(prismaClient);
 
 // https://console.prisma.io/cmdttwfrv01p10ajys0zfvb6g/cmdttycgw01pd0ajybeiu4ljq/cmdttyd7s01pe0ajybu0qrtyr/accelerate/setup
 export const prisma = prismaClient.$extends(withAccelerate());
+
+// https://github.com/prisma/prisma/issues/22050
+// const createPrismaClient = () => {
+//   const prismaClient = new PrismaClient();
+
+//   addEventUpdatedAtMiddleware(prismaClient);
+
+//   // https://console.prisma.io/cmdttwfrv01p10ajys0zfvb6g/cmdttycgw01pd0ajybeiu4ljq/cmdttyd7s01pe0ajybu0qrtyr/accelerate/setup
+//   return prismaClient.$extends(withAccelerate());
+// };
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: ReturnType<typeof createPrismaClient>;
+// };
+
+// const prisma = globalForPrisma.prisma || createPrismaClient();
+
+// export { prisma };
